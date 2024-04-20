d4c18bd8c7df841ee4f285f1875704fe6bb8c03d
abhilash-git <abhilashcs87@gmail.com>
2024-04-21 00:05:31 +0530

    working first1

--- backend/core.py
+++ backend/core.py
@@ -1,27 +1,29 @@
+import os
 from dotenv import load_dotenv
-
-load_dotenv()
-
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-
-from typing import Any, Dict, List
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import RetrievalQA
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_pinecone import PineconeVectorStore
+from const import INDEX_NAME
 
 
-INDEX_NAME = "langchain-doc-index"
-
-
-def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
-    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-    docsearch = PineconeVectorStore(embedding=embeddings, index_name=INDEX_NAME)
-
-    chat = ChatOpenAI(
-        verbose=True,
-        temperature=0,
+def from_llm(query: str) -> str:
+    embeddings = OpenAIEmbeddings()
+    docsearch = PineconeVectorStore.from_existing_index(
+        index_name=INDEX_NAME, embedding=embeddings
+    )
+    chat = ChatOpenAI(verbose=True, temperature=0)
+    qa = RetrievalQA.from_llm(
+        llm=chat,
+        # chain_type="stuff",
+        retriever=docsearch.as_retriever(),
+        return_source_documents=True,
     )
 
-    qa = ConversationalRetrievalChain.from_llm(
-        llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
-    )
-    return qa.invoke({"question": query, "chat_history": chat_history})
+    return qa.invoke({"query": query})
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    query = "what is a langchain"
+    ret = from_llm(query)
+    print(ret)
--- ingestion.py
+++ ingestion.py
@@ -5,27 +5,36 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders.readthedocs import ReadTheDocsLoader
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
-
-
-
+from dotenv import load_dotenv
+from const import INDEX_NAME
 
 
 def ingest_docs():
-    loader = ReadTheDocsLoader(path='./langchain-docs/langchain-docs/', encoding='utf-8', errors=ignore)
+    loader = ReadTheDocsLoader(
+        path="./langchain-docs/langchain-docs/", encoding="utf-8", errors=ignore
+    )
     raw_document = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50, separators=["\n\n", "\n", " ", ""])
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500, chunk_overlap=50, separators=["\n\n", "\n", " ", ""]
+    )
     document = text_splitter.split_documents(documents=raw_document)
     # print(document)
 
     for doc in document:
         old_path = doc.metadata["source"]
-        new_path = old_path.replace("langchain-docs\\langchain-docs\\", 'https://api.python.langchain.com/en/latest/')
+        new_path = old_path.replace(
+            "langchain-docs\\langchain-docs\\",
+            "https://api.python.langchain.com/en/latest/",
+        )
         doc.metadata["source"] = new_path
 
     # print(document)
     embeddings = OpenAIEmbeddings()
-    docsearch = PineconeVectorStore.from_documents(document, embeddings, index_name="documentation-helper")
+    docsearch = PineconeVectorStore.from_documents(
+        document, embeddings, index_name=INDEX_NAME
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    load_dotenv()
     ingest_docs()
