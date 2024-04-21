355f9e7d84c1f96883048cce4bedecf3be53b86a
abhilash-git <abhilashcs87@gmail.com>
2024-04-21 14:47:15 +0530

    chat with history

--- backend/core.py
+++ backend/core.py
@@ -1,30 +1,38 @@
-import os
 from dotenv import load_dotenv
-from langchain.chains import RetrievalQA
+
+# from langchain.chains import RetrievalQA
+from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_pinecone import PineconeVectorStore
 from const import INDEX_NAME
+
 load_dotenv()
 
 
-def from_llm(query: str) -> str:
+def from_llm(query: str, chat_history: list):
     embeddings = OpenAIEmbeddings()
     docsearch = PineconeVectorStore.from_existing_index(
         index_name=INDEX_NAME, embedding=embeddings
     )
     chat = ChatOpenAI(verbose=True, temperature=0)
-    qa = RetrievalQA.from_llm(
-        llm=chat,
-        # chain_type="stuff",
-        retriever=docsearch.as_retriever(),
-        return_source_documents=True,
-    )
+    # qa = RetrievalQA.from_llm(
+    #     llm=chat,
+    #     # chain_type="stuff",
+    #     retriever=docsearch.as_retriever(),
+    #     return_source_documents=True
+    # )
 
-    return qa.invoke({"query": query})
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
+    )
+    print(chat_history)
+    return qa.invoke({"question": query, "chat_history": chat_history})
 
 
 if __name__ == "__main__":
     # load_dotenv()
-    query = "what is a langchain"
-    ret = from_llm(query)
+    test_history = []
+    test_query = "what is a langchain"
+    ret = from_llm(test_query, test_history)
+    test_history.append((test_query, ret))
     print(ret)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from streamlit_chat import message
 from backend.core import from_llm
 from dotenv import load_dotenv
+
 load_dotenv()
 
 st.header("streamlit for documentation helper")
@@ -11,20 +12,40 @@ if "user_input_history" not in st.session_state:
     st.session_state["user_input_history"] = []
 if "bot_answer_history" not in st.session_state:
     st.session_state["bot_answer_history"] = []
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
 
-def create_source_string(in_source:list) -> str:
-    return ''.join([f"{i+1} {in_source[i]} \n" for i in range(len(in_source))])
+
+def create_source_string(in_source: list) -> str:
+    return "".join([f"{i+1} {in_source[i]} \n" for i in range(len(in_source))])
+
 
 if prompt:
     with st.spinner("Generating response.."):
-        generated_response = from_llm(query=prompt)
-        source = list(set([i.metadata['source'] for i in generated_response['source_documents']]))
-        formatted_response = f"{generated_response['result']} \n\n{create_source_string(source)}"
+        generated_response = from_llm(
+            query=prompt, chat_history=st.session_state["chat_history"]
+        )
+        source = list(
+            set([i.metadata["source"] for i in generated_response["source_documents"]])
+        )
+        formatted_response = (
+            f"{generated_response['answer']} \n\n{create_source_string(source)}"
+        )
         # print(formatted_response)
         st.session_state["user_input_history"].append(prompt)
         st.session_state["bot_answer_history"].append(formatted_response)
+        st.session_state["chat_history"].append((prompt, generated_response["answer"]))
 
 if st.session_state:
-    for question, answer in list(reversed(list(zip(st.session_state["user_input_history"],st.session_state["bot_answer_history"])))):
+    for question, answer in list(
+        reversed(
+            list(
+                zip(
+                    st.session_state["user_input_history"],
+                    st.session_state["bot_answer_history"],
+                )
+            )
+        )
+    ):
         message(is_user=True, message=question)
         message(is_user=False, message=answer)
